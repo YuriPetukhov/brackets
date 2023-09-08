9200b795ff2eecee507c634b46ef27c180deaee9
YuriPetukhov <petuchowjuri@yahoo.de>
2023-09-08 18:50:31 +0300

    brackets

---
+++ src/test/java/com/brackets/checkbrackets/CheckBracketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brackets.checkbrackets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckBracketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
